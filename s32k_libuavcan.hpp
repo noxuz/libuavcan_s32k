ff36c792dc6de549a692648eb809ff4c5ab8e4d8
noxuz <abraham.rodriguez@nxp.com>
2019-10-25 22:00:19 -0500

    some formating fixes from IDE glitches

--- s32k_libuavcan.hpp
+++ s32k_libuavcan.hpp
@@ -163,9 +163,9 @@ public:
 				{
 					/* Build up each 32 bit word with 4 indices from frame.data uint8_t array */
 					FlexCAN[ interface_index-1 ]->RAMn[0*MB_Size_Words + MB_Data_Offset + i] = (static_cast<std::uint32_t>(frames[0].data[(i << 2) + 0] << 24))  |
-																							   (static_cast<std::uint32_t>(frames[0].data[(i << 2) + 1] << 16))  |
-																							   (static_cast<std::uint32_t>(frames[0].data[(i << 2) + 2] << 8))   |
-																							 	 	 	 	   	   	      (frames[0].data[(i << 2) + 3] << 0);
+														   (static_cast<std::uint32_t>(frames[0].data[(i << 2) + 1] << 16))  |
+														   (static_cast<std::uint32_t>(frames[0].data[(i << 2) + 2] << 8))   |
+																	      (frames[0].data[(i << 2) + 3] << 0);
 				}
 
 				/* Fill up payload of frame's bytes that dont fill up a 32-bit word, cases of 0,1,2,3,5,6,7 byte data length */
@@ -179,18 +179,18 @@ public:
 
 				/* Fill up word 0 of frame and transmit it
 				 * Extended Data Length       (EDL) = 1
-				 * Bit Rate Switch 		     (BRS) = 1
+				 * Bit Rate Switch 	      (BRS) = 1
 				 * Error State Indicator      (ESI) = 0
-				 * Message Buffer Code	    (CODE) = 12 ( Transmit data frame )
+				 * Message Buffer Code	     (CODE) = 12 ( Transmit data frame )
 				 * Substitute Remote Request  (SRR) = 1  ( Mandatory 1 for Tx )
-				 * ID Extended Bit			 (IDE) = 1
-				 * Remote Tx Request	         (RTR) = 0
-				 * Data Length Code			 (DLC) = frame.getdlc()
+				 * ID Extended Bit	      (IDE) = 1
+				 * Remote Tx Request	      (RTR) = 0
+				 * Data Length Code	      (DLC) = frame.getdlc()
 				 * Counter Time Stamp  (TIME STAMP) = 0 ( Handled by hardware )
 				 */
-				FlexCAN[ interface_index-1 ]->RAMn[0*MB_Size_Words + 0] = CAN_RAMn_DATA_BYTE_1(0x60) 		  |
-																		  CAN_WMBn_CS_DLC(frames[0].getdlc()) |
-																		  CAN_RAMn_DATA_BYTE_0(0xCC);
+				FlexCAN[ interface_index-1 ]->RAMn[0*MB_Size_Words + 0] = CAN_RAMn_DATA_BYTE_1(0x60)          |
+											  CAN_WMBn_CS_DLC(frames[0].getdlc()) |
+											  CAN_RAMn_DATA_BYTE_0(0xCC);
 
 				/* Set the return status as successfull */
 				Status = libuavcan::Result::Success;
@@ -217,9 +217,9 @@ public:
 				{
 					/* Build up each 32 bit word with 4 indices from frame.data uint8_t array */
 					FlexCAN[ interface_index-1 ]->RAMn[1*MB_Size_Words + MB_Data_Offset + i] = (static_cast<std::uint32_t>(frames[0].data[(i << 2) + 0] << 24))  |
-													               	   	   	   	   	   	   	   (static_cast<std::uint32_t>(frames[0].data[(i << 2) + 1] << 16))  |
-																							   (static_cast<std::uint32_t>(frames[0].data[(i << 2) + 2] << 8))	 |
-																							   	   	   	   	   	   	   	  (frames[0].data[(i << 2) + 3] << 0);
+													           (static_cast<std::uint32_t>(frames[0].data[(i << 2) + 1] << 16))  |
+													           (static_cast<std::uint32_t>(frames[0].data[(i << 2) + 2] << 8))   |
+																	      (frames[0].data[(i << 2) + 3] << 0);
 				}
 
 				/* Fill up payload of frame's bytes that dont fill up a 32-bit word, cases of 0,1,2,3,5,6,7 byte data length */
@@ -233,18 +233,18 @@ public:
 
 				/* Fill up word 0 of frame and transmit it
 				 * Extended Data Length       (EDL) = 1
-				 * Bit Rate Switch 		     (BRS) = 1
+				 * Bit Rate Switch 	      (BRS) = 1
 				 * Error State Indicator      (ESI) = 0
-				 * Message Buffer Code	    (CODE) = 12 ( Transmit data frame )
+				 * Message Buffer Code	     (CODE) = 12 ( Transmit data frame )
 				 * Substitute Remote Request  (SRR) = 1  ( Mandatory 1 for Tx )
-				 * ID Extended Bit			 (IDE) = 1
-				 * Remote Tx Request	         (RTR) = 0
-				 * Data Length Code			 (DLC) = frame.getdlc()
-				 * Counter Time Stamp  (TIME STAMP) = 0 ( Handled by hardware )
+				 * ID Extended Bit	      (IDE) = 1
+				 * Remote Tx Request	      (RTR) = 0
+				 * Data Length Code	      (DLC) = frame.getdlc()
+				 * Counter Time Stamp  (TIME STAMP) = 0  ( Handled by hardware )
 				 */
-				FlexCAN[ interface_index-1 ]->RAMn[1*MB_Size_Words + 0] = CAN_RAMn_DATA_BYTE_1(0x60) 			  |
-																		  CAN_WMBn_CS_DLC(frames[0].getdlc()) |
-																		  CAN_RAMn_DATA_BYTE_0(0xCC);
+				FlexCAN[ interface_index-1 ]->RAMn[1*MB_Size_Words + 0] = CAN_RAMn_DATA_BYTE_1(0x60) 	      |
+											  CAN_WMBn_CS_DLC(frames[0].getdlc()) |
+											  CAN_RAMn_DATA_BYTE_0(0xCC);
 
 				/* Set the return status as successfull */
 				Status = libuavcan::Result::Success;
@@ -342,17 +342,17 @@ public:
 
 						/* Setup word 0 (4 Bytes) for ith MB
 						 * Extended Data Length      (EDL) = 1
-						 * Bit Rate Switch 		     (BRS) = 1
+						 * Bit Rate Switch           (BRS) = 1
 						 * Error State Indicator     (ESI) = 0
 						 * Message Buffer Code	    (CODE) = 4 ( Active for reception and empty )
 						 * Substitute Remote Request (SRR) = 0
-						 * ID Extended Bit			 (IDE) = 1
+						 * ID Extended Bit	     (IDE) = 1
 						 * Remote Tx Request	     (RTR) = 0
-						 * Data Length Code			 (DLC) = 0 ( Valid for transmission only )
+						 * Data Length Code	     (DLC) = 0 ( Valid for transmission only )
 						 * Counter Time Stamp (TIME STAMP) = 0 ( Handled by hardware )
 						 */
 						FlexCAN[i]->RAMn[(j+2)*MB_Size_Words] = CAN_RAMn_DATA_BYTE_0(0xC4) |
-																CAN_RAMn_DATA_BYTE_1(0x20);
+											CAN_RAMn_DATA_BYTE_1(0x20);
 
 						/* Setup Message buffers 2-7 29-bit extended ID from parameter */
 						FlexCAN[i]->RAMn[(j+2)*MB_Size_Words + 1] = filter_config[j]->id;
@@ -491,13 +491,12 @@ public:
 	     * LPIT0 channel initialization for timeout timer
          */
 	    PCC->PCCn[PCC_LPIT_INDEX] |= PCC_PCCn_CGC(1); /* Clock gating to LPIT module */
-	    SCG->SIRCDIV |= SCG_SIRCDIV2(4) 			  /* Enable and divide by 8, 1Mhz */
+	    SCG->SIRCDIV |= SCG_SIRCDIV2(4) 		  /* Enable and divide by 8, 1Mhz */
 	    PCC->PCCn[PCC_LPIT_INDEX] |= PCC_PCCn_PCS(2); /* Enable and select SIRCDIV2_CLK (8Mhz) */
-	    LPIT0->MCR |= LPIT_MCR_M_CEN(1); 			  /* Enable module for setup */
+	    LPIT0->MCR |= LPIT_MCR_M_CEN(1); 		  /* Enable module for setup */
 
 	    /* Select 32-bit peridic timer mode (default) */
 	    LPIT0->TMR[3].TCTRL |= LPIT_TMR_TCTRL_MODE(0);
-
 
 		/**
 		 * SysClock initialization for feeding 80Mhz to FlexCAN
@@ -507,7 +506,7 @@ public:
 		SCG->SOSCCSR &= ~SCG_SOSCCSR_LK_MASK;     /* Ensure the register is unlocked */
 		SCG->SOSCCSR &= ~SCG_SOSCCSR_SOSCEN_MASK; /* Disable SOSC for setup */
 		SCG->SOSCCFG  =	 SCG_SOSCCFG_EREFS_MASK | /* Setup external crystal for SOSC reference */
-						 SCG_SOSCCFG_RANGE(2);	  /* Select 8Mhz range */
+				 SCG_SOSCCFG_RANGE(2);	  /* Select 8Mhz range */
 		SCG->SOSCDIV |=  SCG_SOSCDIV_SOSCDIV2(4); /* Divider of 8 for LPIT clock source, gets 1Mhz reference */
 		SCG->SOSCCSR  =  SCG_SOSCCSR_SOSCEN_MASK; /* Enable SOSC reference */
 		SCG->SOSCCSR |=  SCG_SOSCCSR_LK_MASK;	  /* Lock the register from accidental writes */
@@ -516,10 +515,10 @@ public:
 		{
 		  Status = flagPollTimeout_Set(SCG->SOSCCSR,SCG_SOSCCSR_SOSCVLD_MASK);	/* Poll for valid SOSC reference, needs 4096 cycles*/
 
-          /* System PLL (SPLL) initialization for to 160Mhz reference */
+          	  /* System PLL (SPLL) initialization for to 160Mhz reference */
 		  SCG->SPLLCSR &= ~SCG_SPLLCSR_LK_MASK;     /* Ensure the register is unlocked */
 		  SCG->SPLLCSR &= ~SCG_SPLLCSR_SPLLEN_MASK; /* Disable PLL for setup */
-		  SCG->SPLLCFG  =  SCG_SPLLCFG_MULT(24);	/* Select multiply factor of 40 for 160Mhz SPLL_CLK */
+		  SCG->SPLLCFG  =  SCG_SPLLCFG_MULT(24);    /* Select multiply factor of 40 for 160Mhz SPLL_CLK */
 		  SCG->SPLLCSR |=  SCG_SPLLCSR_SPLLEN_MASK; /* Enable PLL */
 		  SCG->SPLLCSR |=  SCG_SPLLCSR_LK_MASK;     /* Lock register from accidental writes */
 
@@ -529,9 +528,9 @@ public:
 
 		    /* Normal RUN configuration for output clocks */
 		    SCG->RCCR    |=  SCG_RCCR_SCS(6)     |    /* Select SPLL as system clock source */
-			     		 	 SCG_RCCR_DIVCORE(1) |	  /* Additional dividers for Normal Run mode */
-				    		 SCG_RCCR_DIVBUS(1)  |
-					    	 SCG_RCCR_DIVSLOW(2);
+			     	     SCG_RCCR_DIVCORE(1) |    /* Additional dividers for Normal Run mode */
+			    	     SCG_RCCR_DIVBUS(1)  |
+			             SCG_RCCR_DIVSLOW(2);
 
 		    /**
 		     * CAN frames timestamping 64-bit timer initialization
@@ -555,18 +554,17 @@ public:
 		    LPIT0->TMR[1].TCTRL |= LPIT_TMR_TCTRL_CHAIN(1);
 
 		    /* Setup max reload value for both channels 0xFFFFFFFF */
-	        LPIT0->TMR[0].TVAL = LPIT_TMR_TVAL_TMR_VAL_MASK;
-	        LPIT0->TMR[1].TVAL = LPIT_TMR_TVAL_TMR_VAL_MASK;
+	            LPIT0->TMR[0].TVAL = LPIT_TMR_TVAL_TMR_VAL_MASK;
+	            LPIT0->TMR[1].TVAL = LPIT_TMR_TVAL_TMR_VAL_MASK;
 
 		    /* Start the timers */
 		    LPIT0->SETTEN |= LPIT_SETTEN_SET_T_EN_0(1) |
-					    	 LPIT_SETTEN_SET_T_EN_1(1);
+				     LPIT_SETTEN_SET_T_EN_1(1);
 
 		    /* Verify that the least significant 32-bit timer is counting (not locked at 0) */
 		    if ( isSuccess(Status) )
 		    {
 			  Status = flagPollTimeoutSet(LPIT0->TMR[0],LPIT_TMR_TVAL_TMR_VAL_MASK);
-
 
 		      /**
 		       * FlexCAN instances initialization
@@ -574,9 +572,9 @@ public:
 		      for(std::uint8_t i = 0; i < S32K_CANFD_Count ; i++)
 		      {
 		        PCC->PCCn[ PCC_FlexCAN_Index[i] ] = PCC_PCCn_CGC_MASK; /* FlexCAN0 clock gating */
-		        FlexCAN[i]->MCR   |=  CAN_MCR_MDIS_MASK;			   /* Disable FlexCAN0 module for clock source selection */
-		        FlexCAN[i]->CTRL1 |=  CAN_CTRL1_CLKSRC_MASK;		   /* Select SYS_CLK as source (80Mhz)*/
-		        FlexCAN[i]->MCR   &= ~CAN_MCR_MDIS_MASK;			   /* Enable FlexCAN and automatic transition to freeze mode for setup */
+		        FlexCAN[i]->MCR   |=  CAN_MCR_MDIS_MASK;	       /* Disable FlexCAN0 module for clock source selection */
+		        FlexCAN[i]->CTRL1 |=  CAN_CTRL1_CLKSRC_MASK;	       /* Select SYS_CLK as source (80Mhz)*/
+		        FlexCAN[i]->MCR   &= ~CAN_MCR_MDIS_MASK;	       /* Enable FlexCAN and automatic transition to freeze mode for setup */
 
 		        /* Block for freeze mode entry */
 		        if ( isSuccess(Status) )
@@ -584,35 +582,35 @@ public:
 	          	  Status = flagPollTimeout_Set(FlexCAN[i]->MCR,CAN_MCR_FRZACK_MASK);
 
 		          /* Next configurations are only permitted in freeze mode */
-		          FlexCAN[i]->MCR |= CAN_MCR_FDEN_MASK  | 	    /* Habilitate CANFD feature */
-				                     CAN_MCR_FRZ_MASK;		    /* Enable freeze mode entry when HALT bit is asserted */
-		          FlexCAN[i]->CTRL2 |= CAN_CTRL2_ISOCANFDEN_MASK; /* Activate the use of ISO 11898-1 CAN-FD standard */
+		          FlexCAN[i]->MCR   |= CAN_MCR_FDEN_MASK  | 	   /* Habilitate CANFD feature */
+				               CAN_MCR_FRZ_MASK;           /* Enable freeze mode entry when HALT bit is asserted */
+		          FlexCAN[i]->CTRL2 |= CAN_CTRL2_ISOCANFDEN_MASK;  /* Activate the use of ISO 11898-1 CAN-FD standard */
 
 		          /* CAN Bit Timing (CBT) configuration for a nominal phase of 1 Mbit/s with 80 time quantas,
 		             in accordance with Bosch 2012 specification, sample point at 83.75% */
 
-		          FlexCAN[i]->CBT |= CAN_CBT_BTF_MASK     |	 /* Enable extended bit timing configurations for CAN-FD
-		 	 	 	 	 	 	 	 	 	 	    			for setting up separetely nominal and data phase */
-		            				 CAN_CBT_EPRESDIV(0)  |	 /* Prescaler divisor factor of 1 */
-									 CAN_CBT_EPROPSEG(46) |	 /* Propagation segment of 47 time quantas */
-							         CAN_CBT_EPSEG1(18)	  |	 /* Phase buffer segment 1 of 19 time quantas */
-									 CAN_CBT_EPSEG2(12)	  |	 /* Phase buffer segment 2 of 13 time quantas */
-									 CAN_CBT_ERJW(12);	     /* Resynchronization jump width same as PSEG2 */
+		          FlexCAN[i]->CBT |= CAN_CBT_BTF_MASK     |  /* Enable extended bit timing configurations for CAN-FD
+		 	 	 	 	 	 	 	for setting up separetely nominal and data phase */
+		            		     CAN_CBT_EPRESDIV(0)  |  /* Prescaler divisor factor of 1 */
+					     CAN_CBT_EPROPSEG(46) |  /* Propagation segment of 47 time quantas */
+					     CAN_CBT_EPSEG1(18)	  |  /* Phase buffer segment 1 of 19 time quantas */
+					     CAN_CBT_EPSEG2(12)	  |  /* Phase buffer segment 2 of 13 time quantas */
+					     CAN_CBT_ERJW(12);	     /* Resynchronization jump width same as PSEG2 */
 
 		          /* CAN-FD Bit Timing (FDCBT) for a data phase of 4 Mbit/s with 20 time quantas,
-		   	   	     in accordance with Bosch 2012 specification, sample point at 75% */
+		   	     in accordance with Bosch 2012 specification, sample point at 75% */
 
 		          FlexCAN[i]->FDCBT |= CAN_FDCBT_FPRESDIV(0) |  /* Prescaler divisor factor of 1 */
-		            				   CAN_FDCBT_FPROPSEG(7) |  /* Propagation semgment of 7 time quantas (only register that doesn't add 1) */
-									   CAN_FDCBT_FPSEG1(6)   |  /* Phase buffer segment 1 of 7 time quantas */
-									   CAN_FDCBT_FPSEG2(4)   |  /* Phase buffer segment 2 of 5 time quantas */
-									   CAN_FDCBT_FRJW(4);       /* Resynchorinzation jump width same as PSEG2 */
+		            		       CAN_FDCBT_FPROPSEG(7) |  /* Propagation semgment of 7 time quantas (only register that doesn't add 1) */
+			    		       CAN_FDCBT_FPSEG1(6)   |  /* Phase buffer segment 1 of 7 time quantas */
+					       CAN_FDCBT_FPSEG2(4)   |  /* Phase buffer segment 2 of 5 time quantas */
+					       CAN_FDCBT_FRJW(4);       /* Resynchorinzation jump width same as PSEG2 */
 
 		          /* Additional CAN-FD configurations */
 		          FlexCAN[i]->FDCTRL |= CAN_FDCTRL_FDRATE_MASK | /* Enable bit rate switch in data phase of frame */
-		            					CAN_FDCTRL_TDCEN_MASK  | /* Enable transceiver delay compensation */
-										CAN_FDCTRL_TDCOFF(5)   | /* Setup 5 FlexCAN clock cycles for delay compensation in data phase sampling */
-										CAN_FDCTRL_MBDSR0(3);    /* Setup 64 bytes per message buffer for a maximum of 7 MB's */
+		            			CAN_FDCTRL_TDCEN_MASK  | /* Enable transceiver delay compensation */
+				   	        CAN_FDCTRL_TDCOFF(5)   | /* Setup 5 FlexCAN clock cycles for delay compensation in data phase sampling */
+					        CAN_FDCTRL_MBDSR0(3);    /* Setup 64 bytes per message buffer for a maximum of 7 MB's */
 
 		          /* Message buffers are located in a dedicated RAM inside FlexCAN, they aren't affected by reset,
 		           * so they must be explicitly initialized, they total 128 slots of 4 words each, which sum to 512 bytes,
@@ -625,8 +623,8 @@ public:
 
 		          /* Setup maximum number of message buffers as 7, 0th and 1st for transmission and 2nd-7th for reception */
 		          FlexCAN[i]->MCR |= CAN_MCR_MAXMB(6)    |
-		            				 CAN_MCR_SRXDIS_MASK | /* Disable self-reception of frames if ID matches */
-									 CAN_MCR_IRMQ_MASK;	 /* Enable individual message buffer masking */
+		            		     CAN_MCR_SRXDIS_MASK | /* Disable self-reception of frames if ID matches */
+					     CAN_MCR_IRMQ_MASK;	   /* Enable individual message buffer masking */
 
 		          /* Setup Message buffers 2-7 for reception and set filters */
 		          for(std::uint8_t j = 0; j < filter_config_length; j++ )
@@ -636,17 +634,17 @@ public:
 
 		            /* Setup word 0 (4 Bytes) for ith MB
 		             * Extended Data Length      (EDL) = 1
-		             * Bit Rate Switch 		     (BRS) = 1
+		             * Bit Rate Switch 		 (BRS) = 1
 		             * Error State Indicator     (ESI) = 0
-		             * Message Buffer Code	    (CODE) = 4 ( Active for reception and empty )
+		             * Message Buffer Code	(CODE) = 4 ( Active for reception and empty )
 		             * Substitute Remote Request (SRR) = 0
-		             * ID Extended Bit			 (IDE) = 1
-		             * Remote Tx Request	     (RTR) = 0
-		             * Data Length Code			 (DLC) = 0 ( Valid for transmission only )
+		             * ID Extended Bit	         (IDE) = 1
+		             * Remote Tx Request	 (RTR) = 0
+		             * Data Length Code          (DLC) = 0 ( Valid for transmission only )
 		             * Counter Time Stamp (TIME STAMP) = 0 ( Handled by hardware )
 		             */
 		            FlexCAN[i]->RAMn[(j+2)*MB_Size_Words] = CAN_RAMn_DATA_BYTE_0(0xC4) |
-		            		  	  	  	  	  	  	  	    CAN_RAMn_DATA_BYTE_1(0x20);
+		            		  	  	  	    CAN_RAMn_DATA_BYTE_1(0x20);
 
 		            /* Setup Message buffers 2-7 29-bit extended ID from parameter */
 		            FlexCAN[i]->RAMn[(j+2)*MB_Size_Words + 1] = filter_config[j]->id;
@@ -673,7 +671,7 @@ public:
 		            }
 
 		          }
-	            }
+	                }
 		      }
 		    }
 		  }
@@ -888,8 +886,8 @@ void CAN0_ORed_0_15_MB_IRQHandler()
 }
 
 #if defined(MCU_S32K146) || defined(MCU_S32K148)
-	void CAN1_ORed_0_15_MB_IRQHandler()
 	/* ISR for FlexCAN1 successful reception) */
+	void CAN1_ORed_0_15_MB_IRQHandler()
 	{
 		/* Callback the static RX Interrupt Service Routine */
 		libuavcan::media::S32K_InterfaceManager::S32K_libuavcan_ISR(1u);
