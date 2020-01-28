0bb9ccf66d10f5933afbaaaa8a9fd668ccd0f826
Abraham <abraham.rodriguez@nxp.com>
2020-01-28 12:38:59 -0600

    Fully tested driver in uavcan node board, additional setup required due to the specific transceiver used, STB pin has internal pullup and required to be set LOW

--- s32k_libuavcan.hpp
+++ s32k_libuavcan.hpp
@@ -46,6 +46,10 @@
  * digital signals are set out to pin headers.
  */
 
+/* Macro for additional configuration needed when using TJA1044 transceiver 
+ * used in NXP's UAVCAN node board, set to 0 when using other boards  */
+#define UAVCAN_NODE_BOARD_USED 1
+
 /* Include desired target S32K14x registers and features header files,
  * defaults to S32K146 from NXP's UAVCAN node board */
 #include "S32K146.h"
@@ -611,9 +615,10 @@ public:
         SCG->SPLLCSR |= SCG_SPLLCSR_SPLLEN_MASK;  /* Enable PLL */
         SCG->SPLLCSR |= SCG_SPLLCSR_LK_MASK;      /* Lock register from accidental writes */
 
+        /* Poll for valid SPLL reference */
         while (!(SCG->SPLLCSR & SCG_SPLLCSR_SPLLVLD_MASK))
         {
-        }; /* Poll for valid SPLL reference */
+        }; 
 
         /* Normal RUN configuration for output clocks */
         SCG->RCCR = SCG_RCCR_SCS(6) |     /* Select SPLL as system clock source */
@@ -751,6 +756,7 @@ public:
             {
             };
 
+            /* Block for module ready flag */
             while (FlexCAN[i]->MCR & CAN_MCR_NOTRDY_MASK)
             {
             };
@@ -765,6 +771,19 @@ public:
         PCC->PCCn[PCC_PORTA_INDEX] |= PCC_PCCn_CGC_MASK; /* Clock gating to PORT A */
         PORTA->PCR[12] |= PORT_PCR_MUX(3);               /* CAN1_RX at PORT A pin 12 */
         PORTA->PCR[13] |= PORT_PCR_MUX(3);               /* CAN1_TX at PORT A pin 13 */
+        
+        /* Set to LOW the standby (STB) pin in both transceivers of the UAVCAN node board */
+        if(UAVCAN_NODE_BOARD_USED)
+        {
+            PORTE->PCR[11] |= PORT_PCR_MUX(1);  /* MUX to GPIO */
+            PTE->PDDR |= 1<<11;                 /* Set direction as output */
+            PTE->PCOR |= 1<<11;                 /* Set the pin LOW */
+            
+            PORTE->PCR[10] |= PORT_PCR_MUX(1);
+            PTE->PDDR |= 1<<10;
+            PTE->PCOR |= 1<<10;
+        }
+        
 #endif
 
 #if defined(MCU_S32K148)
